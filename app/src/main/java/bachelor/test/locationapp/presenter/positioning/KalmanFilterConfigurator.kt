f61e02e44c73da5a7acd9d7c80d724e32fabc46e
maxbauer <max-bauer97@web.de>
2020-08-05 17:29:19 +0200

    Created Branch for CV Model

--- app/src/main/java/bachelor/test/locationapp/presenter/positioning/KalmanFilterConfigurator.kt
+++ app/src/main/java/bachelor/test/locationapp/presenter/positioning/KalmanFilterConfigurator.kt
@@ -9,12 +9,19 @@ import org.apache.commons.math3.linear.ArrayRealVector
 private const val TIME_DELTA = 0.1
 
 class KalmanFilterConfigurator {
+    // x
     private val stateVector = ArrayRealVector(6)
+    // F
     private val stateTransitionMatrix = Array2DRowRealMatrix(6, 6)
+    // B
     private val controlMatrix = Array2DRowRealMatrix(6, 3)
+    // Q
     private val processNoiseMatrix = Array2DRowRealMatrix(6, 6)
+    // P
     private val stateEstimateNoiseMatrix = Array2DRowRealMatrix(6, 6)
+    // H
     private val measurementMatrix = Array2DRowRealMatrix(3, 6)
+    // R
     private val measurementNoiseMatrix = Array2DRowRealMatrix(3, 3)
 
     private lateinit var processModel: DefaultProcessModel
@@ -27,6 +34,7 @@ class KalmanFilterConfigurator {
     }
 
     private fun configureProcessModel(initialLocation: LocationData): DefaultProcessModel {
+        // x
         stateVector.setEntry(0, initialLocation.xPos.toDouble())
         stateVector.setEntry(1, initialLocation.yPos.toDouble())
         stateVector.setEntry(2, initialLocation.zPos.toDouble())
@@ -34,6 +42,7 @@ class KalmanFilterConfigurator {
         stateVector.setEntry(4, 0.0)
         stateVector.setEntry(5, 0.0)
 
+        // P
         stateEstimateNoiseMatrix.setEntry(0, 0, 1.0)
         stateEstimateNoiseMatrix.setEntry(0, 1, 0.0)
         stateEstimateNoiseMatrix.setEntry(0, 2, 0.0)
@@ -71,6 +80,7 @@ class KalmanFilterConfigurator {
         stateEstimateNoiseMatrix.setEntry(5, 4, 0.0)
         stateEstimateNoiseMatrix.setEntry(5, 5, 0.1)
 
+        // F
         stateTransitionMatrix.setEntry(0, 0, 1.0)
         stateTransitionMatrix.setEntry(0, 1, 0.0)
         stateTransitionMatrix.setEntry(0, 2, 0.0)
@@ -108,6 +118,7 @@ class KalmanFilterConfigurator {
         stateTransitionMatrix.setEntry(5, 4, 0.0)
         stateTransitionMatrix.setEntry(5, 5, 1.0)
 
+        // B
         controlMatrix.setEntry(0, 0, 0.5 * (TIME_DELTA * TIME_DELTA))
         controlMatrix.setEntry(0, 1, 0.0)
         controlMatrix.setEntry(0, 2, 0.0)
@@ -127,6 +138,7 @@ class KalmanFilterConfigurator {
         controlMatrix.setEntry(5, 1, 0.0)
         controlMatrix.setEntry(5, 2, TIME_DELTA)
 
+        // Simple Q
         processNoiseMatrix.setEntry(0, 0, 0.0)
         processNoiseMatrix.setEntry(0, 1, 0.0)
         processNoiseMatrix.setEntry(0, 2, 0.0)
@@ -148,24 +160,24 @@ class KalmanFilterConfigurator {
         processNoiseMatrix.setEntry(3, 0, 0.0)
         processNoiseMatrix.setEntry(3, 1, 0.0)
         processNoiseMatrix.setEntry(3, 2, 0.0)
-        processNoiseMatrix.setEntry(3, 3, 0.007)
+        processNoiseMatrix.setEntry(3, 3, 0.0064)
         processNoiseMatrix.setEntry(3, 4, 0.0)
         processNoiseMatrix.setEntry(3, 5, 0.0)
         processNoiseMatrix.setEntry(4, 0, 0.0)
         processNoiseMatrix.setEntry(4, 1, 0.0)
         processNoiseMatrix.setEntry(4, 2, 0.0)
         processNoiseMatrix.setEntry(4, 3, 0.0)
-        processNoiseMatrix.setEntry(4, 4, 0.007)
+        processNoiseMatrix.setEntry(4, 4, 0.0064)
         processNoiseMatrix.setEntry(4, 5, 0.0)
         processNoiseMatrix.setEntry(5, 0, 0.0)
         processNoiseMatrix.setEntry(5, 1, 0.0)
         processNoiseMatrix.setEntry(5, 2, 0.0)
         processNoiseMatrix.setEntry(5, 3, 0.0)
         processNoiseMatrix.setEntry(5, 4, 0.0)
-        processNoiseMatrix.setEntry(5, 5, 0.007)
+        processNoiseMatrix.setEntry(5, 5, 0.0064)
 
-        // Alternative Q
-        processNoiseMatrix.setEntry(0, 0, 0.000025)
+        // Complete covariance Q
+        /*processNoiseMatrix.setEntry(0, 0, 0.000025)
         processNoiseMatrix.setEntry(0, 1, 0.0)
         processNoiseMatrix.setEntry(0, 2, 0.0)
         processNoiseMatrix.setEntry(0, 3, 0.0005)
@@ -200,12 +212,51 @@ class KalmanFilterConfigurator {
         processNoiseMatrix.setEntry(5, 2, 0.0005)
         processNoiseMatrix.setEntry(5, 3, 0.0)
         processNoiseMatrix.setEntry(5, 4, 0.0)
-        processNoiseMatrix.setEntry(5, 5, 0.01)
+        processNoiseMatrix.setEntry(5, 5, 0.01)*/
+
+        // Zero Q
+        /*processNoiseMatrix.setEntry(0, 0, 0.0)
+        processNoiseMatrix.setEntry(0, 1, 0.0)
+        processNoiseMatrix.setEntry(0, 2, 0.0)
+        processNoiseMatrix.setEntry(0, 3, 0.0)
+        processNoiseMatrix.setEntry(0, 4, 0.0)
+        processNoiseMatrix.setEntry(0, 5, 0.0)
+        processNoiseMatrix.setEntry(1, 0, 0.0)
+        processNoiseMatrix.setEntry(1, 1, 0.0)
+        processNoiseMatrix.setEntry(1, 2, 0.0)
+        processNoiseMatrix.setEntry(1, 3, 0.0)
+        processNoiseMatrix.setEntry(1, 4, 0.0)
+        processNoiseMatrix.setEntry(1, 5, 0.0)
+        processNoiseMatrix.setEntry(2, 0, 0.0)
+        processNoiseMatrix.setEntry(2, 1, 0.0)
+        processNoiseMatrix.setEntry(2, 2, 0.0)
+        processNoiseMatrix.setEntry(2, 3, 0.0)
+        processNoiseMatrix.setEntry(2, 4, 0.0)
+        processNoiseMatrix.setEntry(2, 5, 0.0)
+        processNoiseMatrix.setEntry(3, 0, 0.0)
+        processNoiseMatrix.setEntry(3, 1, 0.0)
+        processNoiseMatrix.setEntry(3, 2, 0.0)
+        processNoiseMatrix.setEntry(3, 3, 0.0)
+        processNoiseMatrix.setEntry(3, 4, 0.0)
+        processNoiseMatrix.setEntry(3, 5, 0.0)
+        processNoiseMatrix.setEntry(4, 0, 0.0)
+        processNoiseMatrix.setEntry(4, 1, 0.0)
+        processNoiseMatrix.setEntry(4, 2, 0.0)
+        processNoiseMatrix.setEntry(4, 3, 0.0)
+        processNoiseMatrix.setEntry(4, 4, 0.0)
+        processNoiseMatrix.setEntry(4, 5, 0.0)
+        processNoiseMatrix.setEntry(5, 0, 0.0)
+        processNoiseMatrix.setEntry(5, 1, 0.0)
+        processNoiseMatrix.setEntry(5, 2, 0.0)
+        processNoiseMatrix.setEntry(5, 3, 0.0)
+        processNoiseMatrix.setEntry(5, 4, 0.0)
+        processNoiseMatrix.setEntry(5, 5, 0.0)*/
 
         return DefaultProcessModel(stateTransitionMatrix, controlMatrix, processNoiseMatrix, stateVector, stateEstimateNoiseMatrix)
     }
 
     private fun configureMeasurementModel(): DefaultMeasurementModel {
+        // H
         measurementMatrix.setEntry(0, 0, 1.0)
         measurementMatrix.setEntry(0, 1, 0.0)
         measurementMatrix.setEntry(0, 2, 0.0)
@@ -225,7 +276,8 @@ class KalmanFilterConfigurator {
         measurementMatrix.setEntry(2, 4, 0.0)
         measurementMatrix.setEntry(2, 5, 0.0)
 
-        measurementNoiseMatrix.setEntry(0, 0, 0.1)
+        // R
+        /*measurementNoiseMatrix.setEntry(0, 0, 0.1)
         measurementNoiseMatrix.setEntry(0, 1, 0.0)
         measurementNoiseMatrix.setEntry(0, 2, 0.0)
         measurementNoiseMatrix.setEntry(1, 0, 0.0)
@@ -233,7 +285,7 @@ class KalmanFilterConfigurator {
         measurementNoiseMatrix.setEntry(1, 2, 0.0)
         measurementNoiseMatrix.setEntry(2, 0, 0.0)
         measurementNoiseMatrix.setEntry(2, 1, 0.0)
-        measurementNoiseMatrix.setEntry(2, 2, 0.2)
+        measurementNoiseMatrix.setEntry(2, 2, 0.2)*/
 
         // Alternative R
         measurementNoiseMatrix.setEntry(0, 0, 0.005)
@@ -248,5 +300,4 @@ class KalmanFilterConfigurator {
 
         return DefaultMeasurementModel(measurementMatrix, measurementNoiseMatrix)
     }
-
 }
